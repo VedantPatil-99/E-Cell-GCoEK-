fe9fa646a3591711859d12c1509ddc76e912d458
Vedant Patil <137187890+VedantPatil-99@users.noreply.github.com>
2024-09-06 00:33:15 -0700

    Add files via upload

---
+++ scroll_animation.js
@@ -0,0 +1,16 @@
+//common reveal options to create reveal animations
+ScrollReveal({
+  //reset: true,
+  distance: '100px',
+  duration: 2500,
+  delay: 100
+});
+
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.title', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.subtitle', { delay: 600, origin: 'top' });
+ScrollReveal().reveal('.description', { delay: 700, origin: 'right' });
+ScrollReveal().reveal('.card', { delay: 500, origin: 'bottom', interval: 100 });
+// ScrollReveal().reveal('.cards', { delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.gallery-img', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.footer', { delay: 700, origin: 'bottom', interval: 100 });
